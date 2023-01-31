cdb9b84e4dab05cc9aa42aba5c358881acedaf8e
akshayghuge9990 <ghugeakshay123@gmail.com>
2023-01-31 14:00:25 +0530

    changes by Akshay

---
+++ src/test/java/com/akshay/StudentManagmentsSystem/StudentManagmentsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.StudentManagmentsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
